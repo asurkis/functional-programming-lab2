ea0e557547135360659a5db085df66140f7b350d
Anton Surkis <antonsurkis@gmail.com>
2021-11-01 15:21:05 +0300

    Add basic functions

--- Program.fs
+++ Program.fs
@@ -1,13 +1,62 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+module Graph =
+    type 'T Edge =
+        { Nodes: struct (int * int)
+          Weight: 'T }
 
-open System
+    type 'T Graph =
+        | Empty
+        | Next of struct ('T Edge * 'T Graph)
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+    let edge struct (na, nb) weight =
+        { Nodes = if na < nb then na, nb else nb, na
+          Weight = weight }
+
+    let singleEdge edge = Next(edge, Empty)
+
+    let rec merge left right =
+        match left with
+        | Empty -> right
+        | Next (currl, nextl) ->
+            match right with
+            | Empty -> left
+            | Next (currr, nextr) ->
+                if currl.Nodes < currr.Nodes then
+                    Next(currl, merge nextl right)
+                elif currl.Nodes > currr.Nodes then
+                    Next(currr, merge left nextr)
+                else
+                    Next(currl, merge nextl nextr)
+
+    let rec filter f graph =
+        match graph with
+        | Empty -> Empty
+        | Next (curr, next) ->
+            let filtered = filter f next
+
+            if f curr then
+                Next(curr, filtered)
+            else
+                filtered
+
+    let map f graph =
+        match graph with
+        | Empty -> Empty
+        | Next (curr, next) ->
+            let mapped = f curr
+            Next(edge curr.Nodes mapped, next)
+
+    let rec withEdge edge graph = edge |> singleEdge |> merge graph
+
+    let rec withoutEdge nodes graph =
+        let isNotSameEdge e = e.Nodes <> nodes
+        filter isNotSameEdge graph
 
 [<EntryPoint>]
-let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+let main _ =
+    let graph = Graph.Empty
+
+    let more =
+        Graph.withEdge (Graph.edge struct (1, 2) 5) graph
+
+    printfn "%A" more
+    0
