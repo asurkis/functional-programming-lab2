e7ace5409b755f92605de7046d27f8ea684253e1
Anton Surkis <antonsurkis@gmail.com>
2021-11-02 11:38:17 +0300

    Refactoring

--- PropertyTest1.fs
+++ PropertyTest1.fs
@@ -3,26 +3,26 @@ module Lab2.PropertyTest1
 open FsCheck.NUnit
 open Lab2
 
-let rec validGraph graph =
+let rec isGraphValid graph =
     match graph with
     | Graph.Empty -> true
     | Graph.Next (edge, next) ->
         match next with
         | Graph.Empty -> true
-        | Graph.Next (nextEdge, _) -> edge.Nodes < nextEdge.Nodes && validGraph next
+        | Graph.Next (nextEdge, _) -> edge.Nodes < nextEdge.Nodes && isGraphValid next
 
 [<Property>]
 let ``Graph is equal to itself`` (graph: int Graph.Graph) = graph = graph
 
 [<Property>]
 let ``Union of valid graphs is a valid graph`` (left: int Graph.Graph) (right: int Graph.Graph) =
-    not (validGraph left && validGraph right)
-    || validGraph (Graph.union left right)
+    not (isGraphValid left && isGraphValid right)
+    || isGraphValid (Graph.union left right)
 
 [<Property>]
 let ``Complement of valid graphs is a valid graph`` (left: int Graph.Graph) (right: int Graph.Graph) =
-    not (validGraph left && validGraph right)
-    || validGraph (Graph.complement left right)
+    not (isGraphValid left && isGraphValid right)
+    || isGraphValid (Graph.complement left right)
 
 [<Property>]
 let ``Complement of graphs is complement of union and right graph`` (left: int Graph.Graph) (right: int Graph.Graph) =
@@ -30,12 +30,12 @@ let ``Complement of graphs is complement of union and right graph`` (left: int G
     let compLeft = Graph.complement left right
     let compUnion = Graph.complement union right
 
-    not (validGraph left && validGraph right)
+    not (isGraphValid left && isGraphValid right)
     || (compLeft = compUnion)
 
 [<Property>]
 let ``Union of graphs is associative`` (a: int Graph.Graph) (b: int Graph.Graph) (c: int Graph.Graph) =
-    not (validGraph a && validGraph b && validGraph c)
+    not (isGraphValid a && isGraphValid b && isGraphValid c)
     || Graph.union (Graph.union a b) c = Graph.union a (Graph.union b c)
 
 [<Property>]
@@ -44,5 +44,5 @@ let ``Union of graphs is commutative`` (left: int Graph.Graph) (right: int Graph
     let unionLeft = Graph.union complement right
     let unionRight = Graph.union right complement
 
-    not (validGraph left && validGraph right)
+    not (isGraphValid left && isGraphValid right)
     || unionLeft = unionRight
