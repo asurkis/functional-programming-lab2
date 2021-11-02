2fbbdc6956673fa85ffda76328d27167ca44c397
Anton Surkis <antonsurkis@gmail.com>
2021-11-02 09:27:03 +0300

    Add unit tests

--- UnitTest1.fs
+++ UnitTest1.fs
@@ -8,3 +8,21 @@ let ``Empty graphs are equal`` () =
     let g1 = Graph.Empty
     let g2 = Graph.Empty
     Assert.AreEqual(g1, g2)
+
+[<Test>]
+let ``Empty and non-empty graphs are not equal`` () =
+    let g1 = Graph.Empty
+    let g2 = Graph.edge (1, 2) 3 |> Graph.singleEdge
+    Assert.AreNotEqual(g1, g2)
+
+[<Test>]
+let ``Single element graphs with the same element are equal`` () =
+    let g1 = Graph.edge (1, 2) 3 |> Graph.singleEdge
+    let g2 = Graph.edge (1, 2) 3 |> Graph.singleEdge
+    Assert.AreEqual(g1, g2)
+
+[<Test>]
+let ``Single element graphs with different elements are not equal`` () =
+    let g1 = Graph.edge (1, 2) 3 |> Graph.singleEdge
+    let g2 = Graph.edge (1, 2) 4 |> Graph.singleEdge
+    Assert.AreNotEqual(g1, g2)
